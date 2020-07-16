cf5c90de70d5b73810252b8d3e75d5b566e74281
sneh96 <snehalpatel7095@gmail.com>
2020-07-16 18:19:56 -0400

    first commit

---
+++ src/components/users/Search.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Search = ({ searchUsers, showClear, clearUsers, showAlert }) => {
+  const [text, setText] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === '') {
+      showAlert('Please enter something', 'light');
+    } else {
+      searchUsers(text);
+      setText('');
+    }
+  };
+
+  const onChange = (e) => setText(e.target.value);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='form'>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
+      </form>
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  showAlert: PropTypes.func.isRequired,
+};
+
+export default Search;
